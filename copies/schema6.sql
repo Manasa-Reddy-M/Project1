78e0e66403b3030d37675d8036fc4996f8b992d4
Manasa-Reddy-M <manasa.intern@phonepe.com>
2022-06-02 20:54:26 +0530

    7 commit

--- copies/schema6.sql
+++ copies/schema6.sql
@@ -33,19 +33,6 @@ CREATE TABLE `SREPYMTS` (
   `hobbies` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `demo1`
---
-
-DROP TABLE IF EXISTS `demo1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `demo1` (
-  `name` varchar(40) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -56,4 +43,4 @@ CREATE TABLE `demo1` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-02 20:29:34
+-- Dump completed on 2022-06-02 20:54:26
